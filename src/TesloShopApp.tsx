0ba7d192db619d2b7e8c64a96efb7243c4337235
Carles Labrana <carles.labrana@idealiveconsulting.com>
2025-09-19 00:13:07 +0200

    Agregar la biblioteca 'sonner' y configurar el componente Toaster en la aplicación

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -2,14 +2,16 @@ import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';  
 import { appRouter } from './app.router';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
   return(
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={appRouter} />
-      {/* The rest of your application */}
+        {/* The rest of your application */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
